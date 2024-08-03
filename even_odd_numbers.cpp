5bd04bf60dda1bc8965d51a438a399c0587aa8c0
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-03 09:28:10 +0530

    uploaded even odd

---
+++ even_odd_numbers.cpp
@@ -0,0 +1,22 @@
+//23070123014 Akshit Mathur 
+#include <iostream>
+using namespace std;
+int main() {
+     int num;
+    cout<<"Enter the number:"<<endl;
+    cin>>num;
+    if (num%2==0){
+        cout<<"The number is Even";
+    }
+    else{
+        cout<<"The number is Odd";
+    }
+    return 0;
+}
+//output
+//Enter the number:
+//2
+//The number is Even
+//Enter the number:
+//35
+//The number is Odd
