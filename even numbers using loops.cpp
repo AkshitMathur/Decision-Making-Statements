c8bdf2804f1a8d924160940690707893f6d8dd27
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-03 10:22:10 +0530

    uploaded

---
+++ even numbers using loops.cpp
@@ -0,0 +1,19 @@
+//23070123014 Akshit Mathur
+//AIM - To print even number using loops.
+#include <iostream>
+using namespace std;
+
+int main() {
+ for(int i=0;i<11;i++)
+{if(i%2==0)
+{cout << i<< endl;}}
+
+    return 0;
+}
+//output
+//0
+//2
+//4
+//6
+//8
+//10
