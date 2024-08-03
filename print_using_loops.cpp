2dc8aba521e736e7283db14d77ec9d7a30b39f6d
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-03 10:18:03 +0530

    uploaded printing using loops

---
+++ print_using_loops.cpp
@@ -0,0 +1,19 @@
+//23070123014 Akshit Mathur
+//AIM - To print multiple times using loop
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() { 
+for(int i=0;i<5;i++)
+
+{cout << "hello"<< endl;}
+
+    return 0;
+}
+//output
+//hello
+//hello
+//hello
+//hello
+//hello
